2296d6043b0335e6eed7d7b5bdde677736d2c41a
bePIRRO <manuel.pirro@gmail.com>
2021-10-02 16:30:12 +0200

    create header + style (scss not work?)

--- dc_comics/resources/views/home.blade.php
+++ dc_comics/resources/views/home.blade.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset("css.app.css") }}">
     <title>DC COMICS</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     @include('includes.header')
     <h1>Home</h1>
+
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
