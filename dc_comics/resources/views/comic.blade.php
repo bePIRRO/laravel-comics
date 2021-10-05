88a29486e86df1bd8bdef86c1e0b6bda0ab5f13d
bePIRRO <manuel.pirro@gmail.com>
2021-10-05 18:00:59 +0200

    add info in dynamic comic page

--- dc_comics/resources/views/comic.blade.php
+++ dc_comics/resources/views/comic.blade.php
@@ -27,4 +27,75 @@
             </div>
         </div>
     </div>
-@endsection
+
+    <div class="container-fluid">
+        <div class="container">
+
+            <div class="row info">
+                <div class="col talent">
+                    <h2>Talent</h2>
+                    <hr class="divide">
+                    <div class="row">
+                        
+                        
+                        <div class="artists">
+                            <h4>Art by:</h4>
+
+                            @foreach ($comic["artists"] as $artist)
+                            <span>{{$artist}}</span>,
+                            @endforeach
+                        </div>
+                    </div>
+                    
+                    <hr class="divide">
+                    <div class="row">
+                        
+
+                        <div class="writers">
+                            <h4>Art by:</h4>
+
+                            @foreach ($comic["writers"] as $writer)
+                            <span>{{$writer}}</span>,
+                            @endforeach
+                        </div>
+                    </div>
+                    <hr class="divide">
+                </div>
+                
+                
+                <div class="col">
+                    <h2>Specs</h2>
+                    <hr class="divide">
+                    <div class="row">
+                        
+                        <div>
+                            <h4>Series:</h4>
+
+                            <span>{{$comic["series"]}}</span>
+                        </div>
+                    </div>
+                    
+                    <hr class="divide">
+                    <div class="row">
+                        
+                        <div>
+                            <h4>U.S. Price:</h4>
+
+                           <span>{{$comic["price"]}}</span>
+                        </div>
+                    </div>
+                    <hr class="divide">
+
+                    <div class="row">
+                        
+                        <div>
+                            <h4>On Sale Date:</h4>
+
+                           <span>{{$comic["sale_date"]}}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+            @endsection
